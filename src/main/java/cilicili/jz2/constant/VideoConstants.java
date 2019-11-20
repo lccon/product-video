97a03403b2010d47d2e933049ee740c64aa3331f
lucong <lu>
2019-11-20 14:37:16 +0800

    前端代码修改

--- src/main/java/cilicili/jz2/constant/VideoConstants.java
+++ src/main/java/cilicili/jz2/constant/VideoConstants.java
@@ -11,11 +11,11 @@ public class VideoConstants {
     public static final Integer LIKE_COUNT = 2;
 
     // 存储附件路径
-    public static final String STORAGE_DIR = "/webroot/upload/";
+    public static final String STORAGE_DIR = "/upload/";
     // 存储附件路径
     public static final String CONVERT_UTIL_DIR = "/convertutil/";
     // 音频路径
-    public static final String AUDIO_DIR = "/webroot/audio/";
+    public static final String AUDIO_DIR = "/audio/";
     // 存储附件最大容量
     public static final long MAX_FILE_SIZE = 52428800L;
 }
