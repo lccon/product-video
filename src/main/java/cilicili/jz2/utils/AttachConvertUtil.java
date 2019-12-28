bc1cec3879e1ca19b4171ad6733ed2da627fdd31
lucong <lu>
2019-12-28 14:38:54 +0800

    代码修改

--- src/main/java/cilicili/jz2/utils/AttachConvertUtil.java
+++ src/main/java/cilicili/jz2/utils/AttachConvertUtil.java
@@ -3,10 +3,7 @@ package cilicili.jz2.utils;
 import cilicili.jz2.constant.VideoConstants;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,13 +28,13 @@ public class AttachConvertUtil {
         String realPath = request.getSession().getServletContext().getRealPath(VideoConstants.STORAGE_DIR);
         String audioRealPath = request.getSession().getServletContext().getRealPath(VideoConstants.AUDIO_DIR);
         List<String> command = new ArrayList<>();
-        command.add(convertUtil+"\\ffmpeg.exe");
+        command.add(convertUtil+ File.separator +"ffmpeg.exe");
 
         command.add("-i");
-        command.add(realPath+"\\"+storageFilename);
+        command.add(realPath+ File.separator +storageFilename);
 
         command.add("-i");
-        command.add(audioRealPath+"\\picture.mp3");
+        command.add(audioRealPath+ File.separator +"picture.mp3");
 
         command.add("-filter_complex");
         command.add("[1:a]aloop=loop=-1:size=2e+09[out];[out][0:a]amix");
@@ -48,7 +45,7 @@ public class AttachConvertUtil {
         command.add(String.valueOf(seconds));
 
         command.add("-y");
-        command.add(realPath+"\\"+convertFilename);
+        command.add(realPath+ File.separator +convertFilename);
 
         ProcessBuilder builder = new ProcessBuilder(command);
         Process process = builder.start();
@@ -88,13 +85,13 @@ public class AttachConvertUtil {
         String realPath = request.getSession().getServletContext().getRealPath(VideoConstants.STORAGE_DIR);
         String audioRealPath = request.getSession().getServletContext().getRealPath(VideoConstants.AUDIO_DIR);
         List<String> command = new ArrayList<>();
-        command.add(convertUtil+"\\ffmpeg.exe");
+        command.add(convertUtil+ File.separator +"ffmpeg.exe");
 
         command.add("-i");
-        command.add(realPath+"\\"+storageFilename);
+        command.add(realPath+ File.separator +storageFilename);
 
         command.add("-i");
-        command.add(audioRealPath+"\\picture.mp3");
+        command.add(audioRealPath+ File.separator +"picture.mp3");
 
         command.add("-filter_complex");
         command.add("[1:a]aloop=loop=-1:size=2e+09");
@@ -104,7 +101,7 @@ public class AttachConvertUtil {
         command.add(String.valueOf(seconds));
 
         command.add("-y");
-        command.add(realPath+"\\"+convertFilename);
+        command.add(realPath+ File.separator +convertFilename);
 
         ProcessBuilder builder = new ProcessBuilder(command);
         Process process = builder.start();
@@ -142,14 +139,14 @@ public class AttachConvertUtil {
         String realPath = request.getSession().getServletContext().getRealPath(VideoConstants.STORAGE_DIR);
         String newVideoUrl = "noSound"+videoUrl;
         List<String> command = new ArrayList<>();
-        command.add(convertUtil+"\\ffmpeg.exe");
+        command.add(convertUtil+ File.separator +"ffmpeg.exe");
         command.add("-i");
-        command.add(realPath+"\\"+videoUrl);
+        command.add(realPath+ File.separator +videoUrl);
 
         command.add("-c:v");
         command.add("copy");
         command.add("-an");
-        command.add(realPath+"\\"+newVideoUrl);
+        command.add(realPath+ File.separator +newVideoUrl);
 
         ProcessBuilder builder = new ProcessBuilder(command);
         Process process = builder.start();
